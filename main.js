56c4ace1db4abc09a481407d91ac3a58128a3c41
Yura <frankins636@gmail.com>
2019-12-26 22:07:07 +0200

    Первое сохранение

---
+++ main.js
@@ -0,0 +1,20 @@
+
+let money = 18000;
+let time = 2019-12-31;
+
+let appData = {
+  budget: 'money',
+  timeData: 'time'
+}
+
+let expenses = {
+
+}
+
+let optionalExpenses = {
+
+}
+
+let income = []
+
+let ask = prompt('Введите обязательную статью расходов в этом месяце', '18000');
